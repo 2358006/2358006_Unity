ae5b0964a14fbb14b839dbc401a08a68d3f92e73
김민영 <2358006@mokwon.ac.kr>
2025-03-18 11:22:05 +0900

    report

--- GameDevelop1/HeroBorn/Assets/Scripts/C2/Lec01.cs
+++ GameDevelop1/HeroBorn/Assets/Scripts/C2/Lec01.cs
@@ -1,25 +1,49 @@
 using UnityEngine;
 public class Lec01 : MonoBehaviour
 {
+  // 1. 스크립트 클래스 구성 : 접근제어자 class  클래스명
+  //   - 접근지정자 : public, private, protected
+  //   - 클래스(class) : 클래스명이라고 하는 이름을 객체로 지정
+  //   - 클래스명 : 파일명과 동일, 클래스명은 영문 숫자 기호 사용할 수 있음
+
+  // 2. 주석
+  //   - 정의 : 코드와 별개로 코드의 설명이나 기능등을 나열할 때 사용
+  //   - 종류 
+  //     - // : 한 줄을 주석 처리 사용
+  //     - /* 한 줄 이상 주석 처리 사용 */
+  //     - /// 설명
+  //       /// 이 코드는 테스트 파일입니다.      
+
+  // 1. 변수와 함수의 이해
+  //     - 변수 : 값(VALUE)이 할당되는 이름
+  //             메모리에 일정 공간을 할애 받는 것
+  //     - 형식 : 자료타입(int, float double char String bool)  변수명  =  값;
+
+  // 정수
+  int gold = 1000;
+
+  // 실수
+  float intermWeight = 1.3f;
+  double Weight = 1.5;
+
+  // 조건 : true(맞다) : 1,  false(틀리다) : 0
+  // 문법상 짝 : if, :;(삼항연산자)
+
+  // 문자열 : char (문자 한 자), 문법상 많이 쓰는곳 : Switch case 문
+  //          string (여러문자를 사용할 때)
+  char itemName1 = 'P';
+  string itemName2 = "Potion";
+
+  // 2. 변수에 할당된 데이터는 런타임(Run Time)(게임 도중)에
+  //     얼마든지 접근하여 수정 가능
+  //    ex) type name;
+  //         type name = value;
+
+  // 숫자형 변수
+  int age = 23;
+  int money = -1000;
   void Start()
   {
-    // 정수
-    int gold = 1000;
-    gold = gold - 200;
-
-    // 실수
-    float intermWeight = 1.3f;
-    double Weight = 1.5;
-
-    // 조건 : true(맞다) : 1,  false(틀리다) : 0
-    // 문법상 짝 : if, :; (삼항연산자)
-
-    // 문자열 : char (문자 한 자), 문법상 많이 쓰는곳 : Switch case 문
-    //          string (여러문자를 사용할 때)
-    char itemName1 = 'P';
-    string itemName2 = "Potion";
-
-
     // 콘솔 출력
     Debug.Log("Hello World");
 
@@ -54,6 +78,5 @@ public class Lec01 : MonoBehaviour
 
     Debug.Log("isBoy : " + isBoy);
     Debug.Log("isGirl : " + isGirl);
-
   }
 }
